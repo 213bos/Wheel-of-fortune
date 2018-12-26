a61131dd5c34be8b7ca36ef8fc80ca278a8d7cc3
Andrew Boskovich <AndrewBoskovich@0587397691.wireless.umich.net>
2018-12-25 22:47:25 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,207 @@
+- Andrew Boskovich
+- 62407900
+- To run my submission, type “python3 wheel_of_fortune.py” in your terminal window after navigating your directories to find the folder “Wheel of Python - Andrew Boskovich”
+- You will be asked to enter your name and then the game will begin
+
+- Example interactions:
+Enter your name: Andrew
+Guessed:
+Category is Same Name:
+_____ & _______ ______
+Spin: $950
+Andrew ($0)
+========================================
+Enter your guess: b
+... there 0 B's
+
+...Computer 2 spins...
+
+Guessed: B
+Category is Same Name:
+_____ & _______ ______
+Spin: $600
+Computer 2 ($0)
+========================================
+... there are 3 L's
+
+...Computer 3 spins...
+
+= Computer 3 loses a turn =
+
+Guessed: B,L
+Category is Same Name:
+_LL__ & _______ __L___
+Spin: $2500
+Computer 3 ($0)
+========================================
+... there 0 R's
+
+...Andrew spins...
+
+Guessed: B,L,R
+Category is Same Name:
+_LL__ & _______ __L___
+Spin: $900
+Andrew ($0)
+========================================
+Enter your guess: k
+... there 0 K's
+
+...Computer 2 spins...
+
+Guessed: B,L,R,K
+Category is Same Name:
+_LL__ & _______ __L___
+Spin: $2500
+Computer 2 ($1800)
+========================================
+... there is 1 E
+
+...Computer 3 spins...
+
+Guessed: B,L,R,K,E
+Category is Same Name:
+ELL__ & _______ __L___
+Spin: $650
+Computer 3 ($0)
+========================================
+... there are 2 N's
+
+...Andrew spins...
+
+Guessed: B,L,R,K,E,N
+Category is Same Name:
+ELL__ & __N____ __L_N_
+Spin: $950
+Andrew ($0)
+========================================
+Enter your guess: p
+... there 0 P's
+
+...Computer 2 spins...
+
+Guessed: B,L,R,K,E,N,P
+Category is Same Name:
+ELL__ & __N____ __L_N_
+Spin: $700
+Computer 2 ($4050)
+========================================
+... there is 1 D
+
+...Computer 3 spins...
+
+Guessed: B,L,R,K,E,N,P,D
+Category is Same Name:
+ELL__ & __N____ __L_ND
+Spin: $900
+Computer 3 ($1300)
+========================================
+... there are 3 A's
+
+...Andrew spins...
+
+Guessed: B,L,R,K,E,N,P,D,A
+Category is Same Name:
+ELL__ & _AN_A__ __LAND
+Spin: $2500
+Andrew ($0)
+========================================
+Enter your guess: o
+...Need at least $250. Try again.
+Enter your guess: t
+... there is 1 T
+
+...Computer 2 spins...
+
+Guessed: B,L,R,K,E,N,P,D,A,T
+Category is Same Name:
+ELL__ & _ANTA__ __LAND
+Spin: $2500
+Computer 2 ($4750)
+========================================
+... there 0 X's
+
+...Computer 3 spins...
+
+Guessed: B,L,R,K,E,N,P,D,A,T,X
+Category is Same Name:
+ELL__ & _ANTA__ __LAND
+Spin: $2500
+Computer 3 ($3750)
+========================================
+... there 0 G's
+
+...Andrew spins...
+
+Guessed: B,L,R,K,E,N,P,D,A,T,X,G
+Category is Same Name:
+ELL__ & _ANTA__ __LAND
+Spin: $900
+Andrew ($2500)
+========================================
+Enter your guess: s
+... there are 3 S's
+
+...Computer 2 spins...
+
+Guessed: B,L,R,K,E,N,P,D,A,T,X,G,S
+Category is Same Name:
+ELL_S & _ANTAS_ _SLAND
+Spin: $900
+Computer 2 ($4750)
+========================================
+... there are 2 I's
+
+...Computer 3 spins...
+
+Guessed: B,L,R,K,E,N,P,D,A,T,X,G,S,I
+Category is Same Name:
+ELLIS & _ANTAS_ ISLAND
+Spin: $700
+Computer 3 ($3750)
+========================================
+... there 0 O's
+
+...Andrew spins...
+
+Guessed: B,L,R,K,E,N,P,D,A,T,X,G,S,I,O
+Category is Same Name:
+ELLIS & _ANTAS_ ISLAND
+Spin: $800
+Andrew ($5200)
+========================================
+Enter your guess: g
+... G has already been guessed. Try again
+Enter your guess: c
+... there 0 C's
+
+...Computer 2 spins...
+
+Guessed: B,L,R,K,E,N,P,D,A,T,X,G,S,I,O,C
+Category is Same Name:
+ELLIS & _ANTAS_ ISLAND
+Spin: $950
+Computer 2 ($6300)
+========================================
+... there is 1 Y
+
+...Computer 3 spins...
+
+Guessed: B,L,R,K,E,N,P,D,A,T,X,G,S,I,O,C,Y
+Category is Same Name:
+ELLIS & _ANTASY ISLAND
+Spin: $900
+Computer 3 ($3500)
+========================================
+... there 0 M's
+
+...Andrew spins...
+
+Guessed: B,L,R,K,E,N,P,D,A,T,X,G,S,I,O,C,Y,M
+Category is Same Name:
+ELLIS & _ANTASY ISLAND
+Spin: $650
+Andrew ($5200)
+========================================
+Enter your guess: ELLIS & FANTASY ISLAND
+... Andrew wins $5200 and a trip to Ann Arbor!
---
+++ wheel_of_fortune.py
@@ -0,0 +1,195 @@
+# Andrew Boskovich
+# 62407900
+
+import json
+import wof_computer
+import random
+
+NUM_HUMAN   = 1
+NUM_PLAYERS = 3
+VOWEL_COST  = 250
+VOWELS = ['A', 'E', 'I', 'O', 'U']
+
+# Load the wheel
+wheelFile = open('wheel.json', 'r')
+wheel = json.loads(wheelFile.read())
+wheelFile.close()
+
+# Load the phrase set
+phraseFile = open('phrases.json', 'r')
+phrases = json.loads(phraseFile.read())
+phraseFile.close()
+wof_computer.train(phrases)
+
+# Uncomment these to see the structure of wheel and phrases
+
+# print(json.dumps(wheel,indent=3))
+# print(json.dumps(phrases,indent=3))
+
+# Get a random item from wheel
+def spinWheel():
+    return random.choice(wheel)
+
+# Get a random phrase and category
+def getCategoryAndPhrase():
+    category = random.choice(list(phrases.keys()))
+    phrase   = random.choice(phrases[category])
+    return (category, phrase.upper())
+
+def createPlayer(isComputer, player_num):
+    if isComputer:
+        name = 'Computer {}'.format(player_num)
+    else:
+        name = input('Enter your name: ')
+
+    return WheelOfFortunePlayer(name, isComputer)
+
+# Obscures the phrase
+def obscurePhrase(phrase, guessed):
+    whole_phrase = []
+    phrase = phrase.split() # split the phrase into separate words
+    for word in phrase:
+        obscured_word = ''.join(letter if letter in guessed or not letter.isalnum() else '_' for letter in word)
+        whole_phrase.append(obscured_word)
+    obscured_phrase = ' '.join(whole_phrase) # recombine the list
+    return obscured_phrase
+
+class WheelOfFortunePlayer():
+    def __init__(self, name, isComputer):
+        self.name       = name
+        self.prizeMoney = 0
+        self.prizes     = []
+        if isComputer:
+            self.computer = wof_computer.WOFComputer(difficulty = random.randint(1,9))
+        else:
+            self.computer = False
+
+
+    def addMoney(self, amount):
+        self.prizeMoney += amount
+
+    def subtractMoney(self, amount):
+        return self.addMoney(-amount)
+
+    def goBankrupt(self):
+        self.prizeMoney = 0
+
+    def addPrize(self, prize):
+        self.prizes.append(prize)
+
+    def getMove(self, *args, **kwargs):
+        if(self.computer):
+            return self.computer.getMove(*args, **kwargs)
+        else:
+            return input('Enter your guess: ').upper()
+    def __str__(self):
+        return '{} (${})'.format(self.name, self.prizeMoney)
+
+# print('Random spin result:')
+# wheelPrize = spinWheel()
+# print(wheelPrize)
+#
+# category, phrase = getCategoryAndPhrase()
+# print('\nRandom Category: {}\nRandom Phrase:   {}'.format(category, phrase))
+#
+# # example code to illustrate how getMove works. You should delete this.
+# comp_obscured_phrase = 'R___ ____AR'
+# comp_obscured_category = 'Place'
+#
+# print('\n\nComputer guess for {} ({})'.format(comp_obscured_phrase, comp_obscured_category))
+#
+# computer_1 = wof_computer.WOFComputer(difficulty = random.randint(1,9))
+# computerMove = computer_1.getMove(money=0, category=comp_obscured_category, obscuredPhrase = comp_obscured_phrase,
+#                                     guessed=['P','N','X','R','A','Z','S'], wheelPrize=wheelPrize)
+# print('Computer says: {}'.format(computerMove))
+def guessChar(phrase, guess, guessed):
+        count = phrase.count(guess)
+        if count:
+            if count > 1:
+                print('... there are {} {}\'s\n'.format(count, guess))
+            else:
+                print('... there is 1 {}\n'.format(guess))
+        else:
+            print('... there 0 {}\'s\n'.format(guess))
+        return count
+
+def printWinnings(player):
+    prizes = list(set(player.prizes)) # turning it into a set removes the non unique prizes
+    if not prizes:
+        print('... {} wins ${}'.format(player.name, player.prizeMoney))
+    elif len(prizes) == 1:
+        print('... {} wins ${} and {}'.format(player.name, player.prizeMoney, prizes[0][0].lower() + prizes[0][1:]))
+    elif len(prizes) == 2:
+        print('... {} wins ${}, {}, and {}'.format(player.name, player.prizeMoney, prizes[0][0].lower() + prizes[0][1:-1], prizes[1][0].lower() + prizes[1][1:]))
+    exit()
+
+def playGame():
+    players = [createPlayer(player_num>=NUM_HUMAN, player_num+1) for player_num in range(NUM_PLAYERS)]
+    guessed = []
+    category, phrase = getCategoryAndPhrase()
+    playerIndex = 0
+    player = players[playerIndex]
+
+    while True:
+
+        while True:
+            wheelPrize = spinWheel()
+
+            if wheelPrize['type'] == 'cash':
+                obscuredPhrase = obscurePhrase(phrase, guessed)
+                print('Guessed: {}'.format(','.join(guessed)))
+                print('Category is {}: '.format(category))
+                print(obscuredPhrase)
+                print('Spin: {}'.format(wheelPrize['text']))
+
+                print(player)
+                print('='*40)
+
+                while True:
+                    if player.computer:
+                        guess = player.computer.getMove(player.prizeMoney, category, obscuredPhrase, guessed, wheelPrize)
+                    else:
+                        guess = input('Enter your guess: ').upper()
+                    # Check if it's a letter or a phrase
+                    if len(guess) == 1:
+                        if guess in guessed:
+                            print('... {} has already been guessed. Try again'.format(guess))
+                        elif not guess.isalnum():
+                            print('... {} is not a letter. Try again'.format(guess))
+                        else:
+                            if guess in VOWELS:
+                                if player.prizeMoney < VOWEL_COST:
+                                    print ('...Need at least ${}. Try again.'.format(VOWEL_COST))
+                                    continue
+                                else:
+                                    player.prizeMoney -= VOWEL_COST
+                            numCor = guessChar(phrase, guess, guessed)
+                            guessed.append(guess)
+                            player.addMoney(wheelPrize['value']*numCor)
+                            if wheelPrize['prize']:
+                               player.addPrize(wheelPrize['prize'])
+                            break
+                    elif len(guess) == 0: # invalid
+                        print ('... Not a valid guess. Try again')
+                    else: # phrase
+                        if guess == "PASS":
+                            print ('= {} passes ='.format(player.name))
+                            break
+                        if guess == phrase:
+                            printWinnings(player)
+                        else:
+                            print('{} is not the phrase'.format(guess))
+                            break
+                break
+            elif wheelPrize['type'] == 'bankrupt':
+                print('= {} spins bankrupt =\n'.format(player.name))
+                player.goBankrupt()
+                break
+            elif wheelPrize['type'] == 'loseturn':
+                print('= {} loses a turn = \n'.format(player.name))
+        playerIndex += 1
+        playerIndex = playerIndex%len(players)
+        player = players[playerIndex]
+        print ('...{} spins...\n'.format(player.name))
+
+playGame()
